ed217714393d14cb2bfec20e13367813f687a4ed
Al-Giovinetti <giovinettialessio@gmail.com>
2023-07-05 15:36:46 +0200

    Vue collegato e file json inserito

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Disks</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</head>
+<body>
+    <div id="app">
+        
+    </div>
+    
+    <script src="./script/script.js"></script>
+</body>
+</html>
